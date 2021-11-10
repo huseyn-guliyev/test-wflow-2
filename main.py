b919b02fb3bc78c23c72675f9646dff06ebcdd85
Huseyn Guliyev <quliyevhuseyn767@gmail.com>
2021-11-10 20:52:39 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import pickle
+import requests
+from bs4 import BeautifulSoup
+
+df = pd.read_csv('articles.csv')
+url = 'https://github.com/huseyn-guliyev/test-workflow/data'
+response = requests.get(url)
+w_soup =  BeautifulSoup(response.text, 'html.parser')
+txt = w_soup.find_all('a', attrs={'class':'js-navigation-open Link--primary'})
+if len(txt) == 1:
+    x = 0
+else:
+    x = int(txt[0].get('title').split('-')[-1])
+lst = []
+for i in range(x, x+100): #len(df)
+    try: 
+        url = 'https://az.wikipedia.org/wiki/' + df['title'].iloc[i]
+        response = requests.get(url)
+        w_soup =  BeautifulSoup(response.text, 'html.parser')
+        txt = w_soup.find_all('p')
+        s = ''
+        for j in range(len(txt)):
+            s += txt[j].text.replace('\n', " ")
+            s += ' '
+        lst.append(s)
+        
+    except:
+        lst.append('ERROR')
+
+with open("./data/till_{}.pickle".format(x+100), "wb") as fp:
+    pickle.dump(lst, fp)
+    fp.close()
